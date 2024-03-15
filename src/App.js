2e6584a9a9c872a4bac9a72127bedbe35626fb36
MissingNewt <chelby.sallady@gmail.com>
2024-03-15 11:45:38 -0500

    removing react router dom

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import React, { useState, useEffect } from 'react';
 import './App.css';
 import Home from './Components/Home';
 import ArtNav from './Components/ArtNav';
@@ -7,27 +6,49 @@ import Story from './Components/Story';
 import Contact from './Components/Contact';
 import Shop from './Components/Shop';
 import NoPage from './Components/NoPage';
-import Illustrations from "./Components/Illustrations";
-import Walls from "./Components/Walls";
-import Canvases from "./Components/Canvases";
+import Illustrations from './Components/Illustrations';
+import Walls from './Components/Walls';
+import Canvases from './Components/Canvases';
 
 function App() {
+  const [currentRoute, setCurrentRoute] = useState(window.location.pathname);
 
-  return (
-  <BrowserRouter basename="/">
-    <Routes>
-      <Route path="/" element={<Home />} />
-        <Route path="/works" element={<ArtNav />} />
-          <Route path="/works/wall" element={<Walls />} />
-          <Route path="/works/illustration" element={<Illustrations  />} />
-          <Route path="/works/canvas" element={<Canvases />} />
-        <Route path="/story" element={<Story />} />
-        <Route path="/contact" element={<Contact />} />
-        <Route path="/shop" element={<Shop />} />
-        <Route path="*" element={<NoPage />} />
-    </Routes>
-  </BrowserRouter>
-  );
+  useEffect(() => {
+    const handleRouteChange = () => {
+      setCurrentRoute(window.location.pathname);
+    };
+
+    window.addEventListener('popstate', handleRouteChange);
+
+    return () => {
+      window.removeEventListener('popstate', handleRouteChange);
+    };
+  }, []);
+
+  const renderRoute = () => {
+    switch (currentRoute) {
+      case '/':
+        return <Home />;
+      case '/works':
+        return <ArtNav />;
+      case '/works/wall':
+        return <Walls />;
+      case '/works/illustration':
+        return <Illustrations />;
+      case '/works/canvas':
+        return <Canvases />;
+      case '/story':
+        return <Story />;
+      case '/contact':
+        return <Contact />;
+      case '/shop':
+        return <Shop />;
+      default:
+        return <NoPage />;
+    }
+  };
+
+  return <div>{renderRoute()}</div>;
 }
 
 export default App;
